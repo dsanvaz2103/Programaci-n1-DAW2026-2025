bece939fb4cbd54e20b5db1edf5f844c1a407f1e
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2025-11-03 12:48:49 +0100

    Delete Login.py

--- Login.py
+++
@@ -1,6 +0,0 @@
-print("Introduzca su nombre de usuario")
-nombreValido = "dsv12"
-nombre = input()
-if(nombre == nombreValido):print("¡Bienvenido " + nombreValido + "!")    
-else:print("Usuario no valido")
-
