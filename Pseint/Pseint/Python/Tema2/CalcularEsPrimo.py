4154854f442c135499f6bf187265bf90ce6abaab
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2025-11-06 10:46:20 +0100

    Actualizacion y mejora de ejercicios

---
+++ Pseint/Pseint/Python/Tema2/CalcularEsPrimo.py
@@ -0,0 +1,19 @@
+def esPrimo(numero):
+    # Los números menores o iguales a 1 no son primos
+    if numero <= 1:
+        return False
+
+    # El número 2 es el único primo par
+    if numero == 2:
+        return True
+
+    # Bucle para verificar divisores
+    # Empezamos desde 3
+    i = 3
+    while i <= numero:
+        if numero % i == 0:
+            return False  # Se encontró un divisor, no es primo
+        i += 1
+
+    # Si el bucle termina sin encontrar divisores, el número es primo
+    return True
--- Pseint/Pseint/Python/Tema2/Ejercicio12.py
+++ Pseint/Pseint/Python/Tema2/Ejercicio12.py
@@ -1,7 +1,5 @@
 a = [1,2,3,4,5,6,7,8,9,10]
-
-for i in range(len(a) - 1, -1, -1): 
-    if a[i] % 2 == 0:
-        a.remove(a[i])
-
+for e in a:
+    if e % 2 == 0:
+        a.remove(e)
 print(a)
