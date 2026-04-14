7d7fd1723c2c98f01cb7fbf6b0cc84edd1313189
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-04-14 13:17:41 +0200

    Add files via upload

--- Tema8/Ejercico5/Ejercicio5.java
+++ Tema8/Ejercico5/Ejercicio5.java
@@ -1,6 +1,9 @@
 package Tema8.Ejercico5;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
