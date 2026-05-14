7a9325cfb91c57a3e4363b895afc650e2b2066cb
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-05-14 09:42:20 +0200

    Ejercicio 13

---
+++ Tema9/Ejercicio13/Ejercicio13.java
@@ -0,0 +1,33 @@
+package Tema9.Ejercicio13;
+
+import java.io.*;
+
+public class Ejercicio13 {
+    static void main() {
+        Persona personaFamosa = new Persona("Logan", "Reinos");
+        try {
+            FileOutputStream fi = new FileOutputStream("personaFamosa.dat");
+            ObjectOutputStream ob = new ObjectOutputStream(fi);
+            ob.writeObject(personaFamosa);
+            ob.close();
+
+            System.out.println("Persona guardada... ");
+        } catch (IOException e) {
+            System.out.println("Error al guardar");
+        }
+
+        try {
+            FileInputStream fi = new FileInputStream("personaFamosa.dat");
+            ObjectInputStream ob = new ObjectInputStream(fi);
+            Persona p = (Persona) ob.readObject();
+            ob.close();
+
+            System.out.println("Persona leida: ");
+            System.out.println(p.getNombre() + " " + p.getApellido());
+        }catch (IOException e){
+            System.out.println("Error al leer");
+        }catch (ClassNotFoundException e){
+            System.out.println("Clase no encontrada");
+        }
+    }
+}
---
+++ Tema9/Ejercicio13/Persona.java
@@ -0,0 +1,30 @@
+package Tema9.Ejercicio13;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+    private static final long serialVersion = 1L;
+    String nombre;
+    String apellido;
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
