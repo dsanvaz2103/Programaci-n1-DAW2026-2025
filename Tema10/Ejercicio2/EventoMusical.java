ebb8c7ffd518734e98e8f7f21d62ace9553e2657
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-05-27 11:28:06 +0200

    Update EventoMusical.java

--- Tema10/Ejercicio2/EventoMusical.java
+++ Tema10/Ejercicio2/EventoMusical.java
@@ -3,7 +3,7 @@ package Tema10.Ejercicio2;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 import javax.persistence.*;
 
